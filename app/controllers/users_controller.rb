de887575fc220d599338f2926d373c874442973d
Olga Patrakova <olgapatrakova@outlook.com>
2020-05-18 12:35:55 -0700

    Set login welcome message for a new and an existing user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,23 @@ class UsersController < ApplicationController
   end
 
   def login
+    user = User.find_by(username: params[:user][:username])
+
+    if user.nil?
+      # New User
+      user = User.new(username: params[:user][:username])
+      if ! user.save
+        flash[:error] = "Unable to login"
+        redirect_to root_path
+        return
+      end
+      flash[:success] = "Successfully created new user #{user.username} with ID #{user.id}"
+    else
+      # Existing User
+      flash[:success] = "Successfully logged in as existing user #{user.username}"
+    end
+
+    session[:user_id] = user.id
+    redirect_to root_path
   end
 end
