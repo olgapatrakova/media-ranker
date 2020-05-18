a0512d81e6a0b1d02caf8af4d34c00a04064d2f2
Olga Patrakova <olgapatrakova@outlook.com>
2020-05-18 12:19:44 -0700

    Add user login view, css, tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,4 +11,24 @@ describe UsersController do
     must_respond_with :success
   end
 
+  it "can login a new user" do
+    user = nil
+    expect{
+      user = login()
+    }.must_differ "User.count", 1
+
+    must_respond_with :redirect
+    expect(user).wont_be_nil
+    expect(session[:user_id]).must_equal user.id
+    expect(user.username).must_equal user_hash[:user][:username]
+  end
+
+  it "can login an existing user" do
+    user = User.create(username: "Ed")
+
+    expect{
+      login(user.username)
+    }.wont_change "User.count"
+    expect(session[:user_id]).must_equal user.id
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,5 +17,18 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  def login(username = "Grace")
+    # Arrange
+    user_hash = {
+      user: {
+        username: username
+      }
+    }
+
+    post login_path, params: user_hash
+    user  = User.find_by(username: username)
+    return user
+  end
+    
   # Add more helper methods to be used by all tests here...
 end
