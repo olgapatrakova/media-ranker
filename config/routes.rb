a20be3082f3df1b2311caad745df664d59af1c3a
Olga Patrakova <olgapatrakova@outlook.com>
2020-05-18 11:37:26 -0700

    create user model, controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/login_form'
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "users/current", to: "users#current", as: "current_user"
+
   root to: "homepages#index"
+  
   resources :works
 end
