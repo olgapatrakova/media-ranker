19ad4ccfccb1eaa5330985dfde86d3c9ec9214a4
Olga Patrakova <olgapatrakova@outlook.com>
2020-05-20 17:30:35 -0700

    is_spotlight method, more work model changes, final polishing

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,10 +3,30 @@ class Work < ApplicationRecord
   has_many :users, through: :votes
   validates :title, presence: true, uniqueness: true
 
-  def self.find_top(category)
+  def self.split_and_sort(category)
     category_records = self.where(category: category)
-    # give 10
-    return category_records.slice(0,10)
+    sorted_records = category_records.sort_by{ |record| record.votes.count }.reverse
+    return sorted_records
+  end
+
+  def self.find_top(category)
+    sorted = self.split_and_sort(category)
+    if sorted.size > 10
+      return sorted[0,10]
+    else
+      return sorted
+    end
+  end
+
+  def self.is_spotlight
+    # a simple way to find a spotlight that is huge in terms of time complexity
+    # return Work.all.sort_by{ |record| record.votes.count }.first
+
+    # join works and votes, take only those that present in both tables
+    # group them to 1 table, count votes, return 1 with max value
+    # gained help on that
+    spotlight_id = Work.joins(:votes).group("works.id").order("count_votes_id DESC").limit(1).count("votes.id").keys[0]
+    return Work.find_by(id: spotlight_id)
   end
 
   def self.all_categories
